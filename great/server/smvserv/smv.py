dca0ceff726cf39b5068595eac4abd8932fce49c
Kaiqiang Duan <moonshile@foxmail.com>
2015-05-10 10:42:30 +0800

    finish smv check

--- great/server/smvserv/smv.py
+++ great/server/smvserv/smv.py
@@ -7,22 +7,29 @@ Functions for checking invariants with NuSMV
 @author Kaiqiang Duan <duankq@ios.ac.cn>
 """
 
-from pexpect import spawn, EOF
+from pexpect import spawn, EOF, TIMEOUT
 
 class SMV(object):
-    def __init__(self, smv_path, smv_file, timeout=-1):
+    def __init__(self, smv_path, smv_file):
         super(SMV, self).__init__()
         self.smv_path = smv_path
-        self.process = spawn(smv_path + ' -dcx -int -old ' + smv_file, timeout=timeout)
-        res = self.expect(['NuSMV > '], EOF, TIMEOUT)
+        self.process = spawn(smv_path + ' -dcx -int -old ' + smv_file)
+        self.process.expect(['NuSMV > ', EOF, TIMEOUT])
+        self.process.before
 
     def go_and_compute_reachable(self):
         self.process.send('go\ncompute_reachable\n')
-        res = self.process.expect(['The diameter of the FSM is ', EOF, TIMEOUT])
+        self.process.expect(['The diameter of the FSM is ', EOF, TIMEOUT], timeout=-1)
         res = self.process.expect(['\.\s+NuSMV > ', EOF, TIMEOUT])
         if res == 0:
             return self.process.before
         return '0'
 
     def check(self, invariant):
-        pass
+        self.process.send('check_invar -p \"' + invariant + '\"\n')
+        self.process.expect(['-- invariant .* is ', 'ERROR: ', EOF, TIMEOUT], timeout=1)
+        self.process.before
+        res = self.process.expect(['\s*NuSMV > ', EOF, TIMEOUT], timeout=1)
+        if res == 0:
+            return self.process.before
+        return '0'
