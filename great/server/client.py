e499f2eb1205db0229d7d87f67ef8a015594e031
Kaiqiang Duan <moonshile@foxmail.com>
2015-07-03 15:42:42 +0800

    fix bug of generating invs using negation form

--- great/server/client.py
+++ great/server/client.py
@@ -8,8 +8,8 @@ PORT = 50008
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-to_check = '3,1,n_flash_nodata,(!((Sta.Proc.CacheState[0] = cache_s) & (!Sta.Dir.HeadVld = TRUE)))'
-quit = '7,1,n_flash_nodata'
+to_check = '3,1,n_flash_nodata,(!((Sta.Proc.CacheState[2] = cache_e) & (Sta.Proc.ProcCmd[1] = node_getx) & (Sta.UniMsg.Cmd[1] = uni_putx)))'
+quit = '7,1,n_g2k'
 s.sendall('%d,%s'%(len(to_check), to_check))
 data = s.recv(1024)
 print data
