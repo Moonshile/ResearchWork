b53001db871d8f00f4914e0eb4a4f7c439421ba1
Kaiqiang Duan <moonshile@foxmail.com>
2015-07-30 09:18:30 +0800

    flash_data

--- great/examples/flash_data.ml
+++ great/examples/flash_data.ml
@@ -419,5 +419,7 @@ let protocol = {
   properties;
 }
 
-let () = run_with_cmdline (find ~protocol ~murphi:(In_channel.read_all "n_flash_data.m"))
+let () = run_with_cmdline (find ~protocol
+  ~smv:(In_channel.read_all "flash_nodata.smv")
+  ~murphi:(In_channel.read_all "n_flash_data.m"))
 
